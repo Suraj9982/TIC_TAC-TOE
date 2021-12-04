06272511d04bf17f9e334effcce1f50e94eb9161
suraj9982 <surajsk9982@gmail.com>
2021-12-04 11:01:19 +0530

    [suraj] add :  It makes the player to choose X or Y

--- TICTACTOE/TICTACTOE/TicTacToeGame.cs
+++ TICTACTOE/TICTACTOE/TicTacToeGame.cs
@@ -13,25 +13,25 @@ namespace TICTACTOE
             char[] board = new char[10];
             for(int i = 1; i <= board.Length; i++)
             {
-                Console.WriteLine("Enter the spaces of board");
                 board[i] = ' ';
             }
-            string player1, player2;
-            int player = 1;
-            Console.WriteLine("Enter the player1 name");
-            player1 = Console.ReadLine();
-            Console.WriteLine("Enter the player2 name");
-            player2 = Console.ReadLine();
-            if (player % 2 == 0)
+            
+        }
+        public char ChooseInput()
+        {
+            Console.WriteLine("Enter the input X Or Y");
+            char Input = Convert.ToChar(Console.ReadLine());
+            char choose = char.ToUpper(Input);
+            if (choose == 'X')
             {
-                Console.WriteLine("palyer 1 is chance");
-                player1 = "X";
+                return Input;
+            }
+            if (choose == 'O')
+            {
+                return Input;
             }
             else
-            {
-                Console.WriteLine("palyer 2 is chance");
-                player2 = "O";
-            }
+                return 'w';
         }
     }
 }
