14ba4963195fb84b2021068b6e7f518f20049bd8
suraj9982 <surajsk9982@gmail.com>
2021-12-04 09:41:19 +0530

    [suraj] add : Create a game board of 10 size

---
+++ TICTACTOE/TICTACTOE/TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TICTACTOE
+{
+    class TicTacToeGame
+    {
+        public void GameBoard()
+        {
+            char[] board = new char[10];
+            for(int i = 1; i <= board.Length; i++)
+            {
+                Console.WriteLine("Enter the spaces of board");
+                board[i] = ' ';
+            }
+        }
+    }
+}
